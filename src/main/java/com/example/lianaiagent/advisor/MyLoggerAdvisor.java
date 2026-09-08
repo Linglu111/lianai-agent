88f05e7f6ed70f2e585ffce59cf462d55ca8154a
Linglu111 <2731208115@qq.com>
2026-09-08 10:39:09 +0800

    自定义 advisor

--- src/main/java/com/example/lianaiagent/advisor/MyLoggerAdvisor.java
+++ src/main/java/com/example/lianaiagent/advisor/MyLoggerAdvisor.java
@@ -1,4 +1,61 @@
 package com.example.lianaiagent.advisor;
 
-public class MyloggerAdvisor {
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import org.springframework.ai.chat.client.ChatClientMessageAggregator;
+import org.springframework.ai.chat.client.ChatClientRequest;
+import org.springframework.ai.chat.client.ChatClientResponse;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisor;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisorChain;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisor;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisorChain;
+
+
+/**
+ * 自定义日志
+ */
+@Slf4j
+public class MyLoggerAdvisor implements CallAdvisor, StreamAdvisor {
+
+
+    @Override
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+
+    @Override
+    public ChatClientResponse adviseCall(ChatClientRequest chatClientRequest, CallAdvisorChain callAdvisorChain) {
+        logRequest(chatClientRequest);
+
+        ChatClientResponse chatClientResponse = callAdvisorChain.nextCall(chatClientRequest);
+
+        logResponse(chatClientResponse);
+
+        return chatClientResponse;
+    }
+
+    @Override
+    public Flux<ChatClientResponse> adviseStream(ChatClientRequest chatClientRequest,
+                                                 StreamAdvisorChain streamAdvisorChain) {
+        logRequest(chatClientRequest);
+
+        Flux<ChatClientResponse> chatClientResponses = streamAdvisorChain.nextStream(chatClientRequest);
+
+        return new ChatClientMessageAggregator().aggregateChatClientResponse(chatClientResponses, this::logResponse);
+    }
+
+    private void logRequest(ChatClientRequest request) {
+        log.info("AI Request: {}", request.context());
+    }
+
+    private void logResponse(ChatClientResponse chatClientResponse) {
+        log.info("AI Response: {}", chatClientResponse.chatResponse().getResult().getOutput().getText());
+    }
+
 }
