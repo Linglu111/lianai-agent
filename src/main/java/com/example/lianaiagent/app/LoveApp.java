723a4f8cd339dac4b1aaaf41e455a3c38d957b5d
Linglu111 <2731208115@qq.com>
2026-09-09 15:12:14 +0800

    prompt模板

--- src/main/java/com/example/lianaiagent/app/LoveApp.java
+++ src/main/java/com/example/lianaiagent/app/LoveApp.java
@@ -1,37 +1,36 @@
 package com.example.lianaiagent.app;
 
 import com.example.lianaiagent.advisor.MyLoggerAdvisor;
-import com.example.lianaiagent.advisor.ReReadingAdvisor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 @Slf4j
 public class LoveApp {
 
-    private static final String SYSTEM_PROMPT = "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。" +
-            "围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；" +
-            "恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。" +
-            "引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。";
-
     private final ChatClient chatClient;
 
-    public LoveApp(ChatModel dashscopeChatModel) {
-        // 初始化基于内存的对话记忆
-        ChatMemory chatMemory = MessageWindowChatMemory.builder()
-                .maxMessages(10)
-                .build();
+    private final PromptTemplate loveReportPromptTemplate;
+
+    public LoveApp(ChatModel dashscopeChatModel,
+                   ChatMemory chatMemory,
+                   @Value("classpath:/prompts/love-system.st") Resource systemPrompt,
+                   @Value("classpath:/prompts/love-report.st") Resource loveReportPrompt) {
+        this.loveReportPromptTemplate = new PromptTemplate(loveReportPrompt);
 
         chatClient = ChatClient.builder(dashscopeChatModel)
-                .defaultSystem(SYSTEM_PROMPT)
+                .defaultSystem(systemPrompt)
                 .defaultAdvisors(
                         MessageChatMemoryAdvisor.builder(chatMemory).build(),
                         // 自定义拦截器
@@ -65,10 +64,12 @@ public class LoveApp {
      * @return
      */
     public LoveReport doChatWithReport(String message, String chatId){
+        String reportPrompt = loveReportPromptTemplate.render(Map.of("message", message));
+
         LoveReport loveReport = this.chatClient
-                .prompt(SYSTEM_PROMPT + "每次对话后都要生产恋爱结果，标题为{用户名}的恋爱报告，内容为建议列表")
+                .prompt()
                 .advisors(advisor -> advisor.param(ChatMemory.CONVERSATION_ID, chatId))
-                .user(message)
+                .user(reportPrompt)
                 .call()
                 .entity(LoveReport.class);
 
