7023c4c9719dd8811890503f2489bcd10af1ecf8
Linglu111 <2731208115@qq.com>
2026-09-11 16:58:26 +0800

    PgVector

---
+++ src/main/java/com/example/lianaiagent/config/PgVectorVectorStoreConfig.java
@@ -0,0 +1,28 @@
+package com.example.lianaiagent.config;
+
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * pgvector 向量存储配置
+ */
+@Configuration
+public class PgVectorVectorStoreConfig {
+
+    @Bean
+    public VectorStore pgVectorVectorStore(JdbcTemplate jdbcTemplate, EmbeddingModel dashscopeEmbeddingModel){
+        VectorStore vectorStore = PgVectorStore.builder(jdbcTemplate, dashscopeEmbeddingModel)
+                .distanceType(PgVectorStore.PgDistanceType.COSINE_DISTANCE)
+                .indexType(PgVectorStore.PgIndexType.HNSW)
+                .initializeSchema(true)
+                .schemaName("public")
+                .vectorTableName("vector_table")
+                .maxDocumentBatchSize(10000)
+                .build();
+        return vectorStore;
+    }
+}
